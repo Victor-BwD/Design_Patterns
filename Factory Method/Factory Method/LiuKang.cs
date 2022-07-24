3d752c637858879391ff54656e3e4102212aeadc
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2022-07-24 17:44:28 -0300

    Create and implementing interface IPersonagem

---
+++ Factory Method/Factory Method/LiuKang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method
+{
+    public class LiuKang : IPersonagem
+    {
+        void IPersonagem.Escolhido()
+        {
+            Console.WriteLine("Liu Kang");
+        }
+    }
+}
---
+++ Factory Method/Factory Method/Scorpion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method
+{
+    internal class Scorpion : IPersonagem
+    {
+        void IPersonagem.Escolhido()
+        {
+            Console.WriteLine("Scorpion");
+        }
+    }
+}
