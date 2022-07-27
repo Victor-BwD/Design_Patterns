4b06520524e6834a9207bd4d4956c7a2d27122c3
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2022-07-27 00:32:32 -0300

    Interfaces and class EnergiaBaseTerran

---
+++ Abstract_Factory/Abstract_Factory/EnergiaBaseTerran.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory
+{
+    public class EnergiaBaseTerran : IEnergia
+    {
+        public void Composicao()
+        {
+            Console.WriteLine("Energia de sustentação da base mecânica");
+        }
+    }
+}
