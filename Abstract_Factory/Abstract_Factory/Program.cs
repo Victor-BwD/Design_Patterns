1802afa9ccbf177e634e8cb32d38a923c4df01e6
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2022-07-28 03:13:35 -0300

    Abstract factory finished

--- Abstract_Factory/Abstract_Factory/Program.cs
+++ Abstract_Factory/Abstract_Factory/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Abstract_Factory.IFabricaBases fabrica;
+
+Console.WriteLine("Escolha um dos personagens: 1-Protoss | 2-Zergs | 3-Terranos");
+
+switch (Console.ReadLine())
+{
+    case "1":
+        fabrica = new Abstract_Factory.FabricaBaseProtoss();
+        break;
+    case "2":
+        fabrica = new Abstract_Factory.FabricaBaseZerg();
+        break;
+    case "3":
+        fabrica = new Abstract_Factory.FabricaBaseTerran();
+        break;
+}
+Console.ReadLine();
