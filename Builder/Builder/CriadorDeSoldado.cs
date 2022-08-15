d1c453bbf0ed71f4b9644685b33ec26966ca947e
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2022-08-15 01:33:57 -0300

    Builder logic creation

---
+++ Builder/Builder/CriadorDeSoldado.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public abstract class CriadorDeSoldado
+    {
+        protected Soldado _soldado;
+
+        public Soldado ObterSoldado()
+        {
+            return _soldado;
+        }
+
+        public abstract void Arma();
+        public abstract void Transporte();
+        public abstract void Foco();
+    }
+}
---
+++ Builder/Builder/SoldadoInfantariaLeve.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class SoldadoInfantariaLeve : Soldado {
+        public override void DefinirFoco(string foco) {
+            Foco = foco;
+        }
+
+        public override void EscolherArma(string arma) {
+            Arma = arma;
+        }
+
+        public override void EscolherTransporte(string transporte) { 
+            Transporte = transporte;
+        }
+    }
+}
