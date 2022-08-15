de086c07bcce295f6a1ff2eeecc52ceaeda22940
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2022-08-15 01:12:57 -0300

    Singleton project

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,10 @@
+﻿Singleton.Singleton jogador_1 = Singleton.Singleton.GetInstance;
+jogador_1.Mensagem("Jogador 1: A bola está comigo no meio campo.");
+
+Singleton.Singleton jogador_2 = Singleton.Singleton.GetInstance;
+jogador_2.Mensagem("Jogador 2: Recebeu a bola");
+
+Singleton.Singleton jogador_3 = Singleton.Singleton.GetInstance;
+jogador_3.Mensagem("Jogador 3: Recebeu o lançamento na linha de fundo.");
+
+Console.ReadKey();
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton {
+        private static Singleton instance = null;
+
+        public static Singleton GetInstance {
+            get {
+                if(instance == null) {
+                    instance = new Singleton();
+                    Console.WriteLine("Bola em jogo");
+                }
+                return instance;
+            }
+        }
+
+        public void Mensagem(string msg) {
+            Console.WriteLine(msg);
+        }
+    }
+}
